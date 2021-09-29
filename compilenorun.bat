538ba12e5c8f9fac14abce4337cdea1810b9067d
Matacrat <48725508+Matacrat@users.noreply.github.com>
2021-09-29 10:03:20 -0400

    improv 2

---
+++ compilenorun.bat
@@ -0,0 +1,6 @@
+ REM sets the location of gzdoom.
+cd /d C:\Users\Mathew\Documents\GZDoom
+REM Runs the project
+gzdoom.exe -nostartup -norun -noautoload -file C:\Users\Mathew\Documents\GitHub\NULL\src +vid_fullscreen 0 +map map01
+
+exit
