570daada8cd0dc62135d8eff65c829f9297a2d73
Matacrat <48725508+Matacrat@users.noreply.github.com>
2021-10-11 18:39:34 -0400

    nano niko

--- compile.bat
+++ compile.bat
@@ -1,6 +1,7 @@
+@echo off 
  REM sets the location of gzdoom.
 cd /d C:\Users\Mathew\Documents\GZDoom
 REM Runs the project
-gzdoom.exe -nostartup -noautoload -file C:\Users\Mathew\Documents\GitHub\Nanowadmo21\src +vid_fullscreen 0 +map null  +developer 2 +idmypos 1 +vid_fps 1 +noise 0
+gzdoom.exe -nostartup -noautoload -file C:\Users\Mathew\Documents\GitHub\nanowadmo21\src +vid_fullscreen 0 +map null  +developer 2 +idmypos 1 +vid_fps 1 +noise 0
 
 exit
